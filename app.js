f5edda8d2a73fddacf545a7730a241b6c6edff05
lgh <rjslsld92@naver.com>
2020-09-08 12:28:25 +0900

    test 파일 추가 ( js , css )

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express');
 
+const { dirname } = require('path');
+const path = require('path');
+
 var app = express();
 
+/* 
 app.get('/', function(req,res){
     res.send('HELLO WORLD');
 });
+ */
+
+/* 
+app.use(express.static(__dirname + '/public'));
+ */
+
+app.use(express.static(path.join(__dirname,'public')));
+
+console.log(__dirname);
+console.log(path.join(__dirname , 'public'));
 
 app.listen(3000, function(){
     console.log('SERVER ON');
