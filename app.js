5013e8d1f1dc15afd92e6078f883998f7ffcf3b1
lgh <rjslsld92@naver.com>
2020-09-09 23:47:22 +0900

    body-parser install, Insomnia 를 이용한 RESTful API 테스트 ( updatedAt 데이터가 잘 안되서 createAt 이랑 같게 해서 테스트 진행 )

--- app.js
+++ app.js
@@ -1,133 +1,84 @@
+// import modules
 var express = require('express');
-
-const { dirname } = require('path');
-const path = require('path');
 var app = express();
-
+var path = require('path');
 var mongoose = require('mongoose');
-/* 
-mongoose.connect("mongodb+srv://test_user:test_user@mongolgh.qvo0f.mongodb.net/test?retryWrites=true&w=majority");
- */
-/* 환경변수에 등록 한 후 사용 */
-console.log(process.env.MONGO_DB);
+var bodyParser = require('body-parser');
+
+// connect database
 mongoose.connect(process.env.MONGO_DB, {useUnifiedTopology: true, useNewUrlParser: true });
-
-const db = mongoose.connection;
-
-// mongoDB 연동 실패 시 에러 메시지 출력
-db.on('error', function(err){
-    console.log("DB ERROR",err);
-});  
-
-// mongoDB 연동 성공 시 메시지 출력
-db.once('open', function(){
-  console.log('mongoDB CONNECTED');
+var db = mongoose.connection;
+db.once("open", function () {
+    console.log("DB connected!");
+});
+db.on("error", function (err) {
+    console.log("DB ERROR :", err);
 });
 
-/* mongoDB schema 생성 */
-var dataSchema = mongoose.Schema({
-    name : String,
-    count : Number
+// model setting
+var postSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
 });
+var Post = mongoose.model('post', postSchema);
 
-/* mongoDB model 생성 */
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name : "myData"} , function(err, data){
-    if(err){
-        return console.log("Data ERROR : ",err);
-    }
-    if(!data){
-        Data.create({name : "myData", count : 0}, function(err, data){
-            if(err){
-                return console.log("Data ERROR", err);
-            }
-            console.log("Counter initialized : ", data);
-        });
-    }
-});
-
-
-
-/* 
-app.get('/', function(req,res){
-    res.send('HELLO WORLD');
-});
- */
-
-/* path.join 을 이용해서 경로 붙일 수 있음.
-app.use(express.static(__dirname + '/public'));
- */
-
+// view setting
 app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
 
-var data = {count : 0};
+// set middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.get('/', function(req,res){
-    Data.findOne({name : "myData"}, function(err, data){
-        if(err){
-            return console.log("Data ERROR : ", err);
-        }
-        data.count++;
-        data.save(function(err){
-            if(err){
-                return console.log("Data ERROR : ", err);
-            }
-            res.render('my_first_ejs', data);
-        });
+// set routes
+app.get('/posts', function (req, res) {
+    Post.find({}, function (err, posts) {
+        if (err){
+            return res.json({ success: false, message: err });
+        } 
+        res.json({ success: true, data: posts });
     });
-});
+}); // index
 
-app.get('/reset', function(req,res){
-    setCounter(res, 0);
-});
-
-app.get('/set/count', function(req,res){
-    if(req.query.count){
-        setCounter(res, req.query.count);
-    }else{
-        getCounter(res);
-    }
-});
-
-app.get('/set/:num', function(req,res){
-    if(req.params.num){
-        setCounter(res, req.params.num);
-    }else{
-        getCounter(res);
-    }
-});
-
-function setCounter(res, num){
-    console.log("setCounter");
-    Data.findOne({name : "myData"}, function(err, data){
-        if(err){
-            return console.log("Data ERROR : ", err);
-        }
-        data.count = num;
-        data.save(function(err){
-            if(err){
-                return console.log("Data ERROR : ", err);
-            }
-            res.render('my_first_ejs', data);
-        });
+app.post('/posts', function (req, res) {
+    Post.create(req.body.post, function (err, post) {
+        if (err){
+            return res.json({ success: false, message: err });
+        } 
+        res.json({ success: true, data: post });
     });
-}
-
-function getCounter(res){
-    console.log("getCounter");
-    Data.findOne({name : "myData"}, function(err, data){
-        if(err){
-            return console.log("Data ERROR : ", err);
-        }
-        res.render('my_first_ejs', data);
+}); // create
+ 
+app.get('/posts/:id', function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        if (err){
+            return res.json({ success: false, message: err });
+        } 
+        res.json({ success: true, data: post });
     });
-}
+}); // show
 
-/* 디렉토리 경로 
-console.log(__dirname);
-console.log(path.join(__dirname , 'public'));
- */
-app.listen(3000, function(){
-    console.log('SERVER ON');
+app.put('/posts/:id', function (req, res) {
+    //req.body.post.updatedAt = Date.now();
+    Post.findByIdAndUpdate(req.params.id, req.body.post, function (err, post) {
+        if (err){
+            return res.json({ success: false, message: err });
+        } 
+        res.json({ success: true, message: post._id + " updated" });
+    });
+}); //update
+
+app.delete('/posts/:id', function (req, res) {
+    Post.findByIdAndRemove(req.params.id, function (err, post) {
+        if (err){
+            return res.json({ success: false, message: err });
+        } 
+        res.json({ success: true, message: post._id + " deleted" });
+    });
+}); //destroy
+ 
+// start server
+app.listen(3000, function () {
+    console.log('Server On!');
 });
